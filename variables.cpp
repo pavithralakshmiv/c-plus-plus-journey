bf05b7b5a08603b02aff943aecb4aa6b8ad8bbc4
Pavithra Lakshmi V <273815497+pavithralakshmiv@users.noreply.github.com>
2026-04-06 18:20:39 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int num=12;
+void sum()
+{
+    int a;
+    cout<<num;
+}
+int main()
+{
+    int num=20;
+    num=50;
+    int a= 10;
+    int b= 15;
+    float pi= 3.14;
+    char c= 'p';
+    bool is_true= true;
+    bool is_false= false;
+    cout<<"Hi,Welcome.\nHere is the value of a: "<<a<<"\nHere is the value of b: "<<b;
+    cout<<"\nHere is the value of pi: "<<pi;
+    cout<<"\nHere is the value of c: "<<c;
+    cout<<"\nHere is the value of num: "<<num<<endl;
+    cout<<is_true<<endl;
+    cout<<is_false<<endl;
+    return 0;
+
+}
